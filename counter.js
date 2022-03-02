22a487338eae664d67310cb4f44bb2f7a5bd9bcd
Karl Penzhorn <karlpenzhorn@gmail.com>
2022-03-02 08:56:29 +0200

    added some notes

--- counter.js
+++ counter.js
@@ -24,3 +24,27 @@ let counter2 = Counter(1500);
 counter0.parent = document.querySelector("#root0");
 counter1.parent = document.querySelector("#root1");
 counter2.parent = document.querySelector("#root2");
+
+// -- notes --
+
+// we could also use auto to create / add these to the dom
+
+// auto({
+
+//     counter0: Counter(500),
+//     counter1: Counter(1000),
+//     counter2: Counter(1500),
+
+//     add0: (_) => _.counter0 ? _.counter0.parent = document.querySelector("#root0") : [],
+//     add1: (_) => _.counter1 ? _.counter1.parent = document.querySelector("#root1") : [],
+//     add2: (_) => _.counter2 ? _.counter2.parent = document.querySelector("#root2") : []
+
+// })
+
+// why?
+
+// well, let's say you have a variable in the browser for the interval ( perhaps
+// a slider). then you can tie this to the interval creation - as you change
+// the slider new elements are created, and then automatically added to
+// the parent.
+
